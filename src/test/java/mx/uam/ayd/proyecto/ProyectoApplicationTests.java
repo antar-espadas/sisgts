5fdd75d2882d98ebc00398953443f6e12d065a5d
espadas-antar <espadas.antar@gmail.com>
2022-12-07 23:38:46 -0600

    Tercer intento

--- src/test/java/mx/uam/ayd/proyecto/ProyectoApplicationTests.java
+++ src/test/java/mx/uam/ayd/proyecto/ProyectoApplicationTests.java
@@ -3,12 +3,11 @@ package mx.uam.ayd.proyecto;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.condition.DisabledIf;
-import org.junit.jupiter.api.condition.DisabledIfSystemProperty;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.awt.*;
 
-@DisabledIf("GraphicsEnvironment.isHeadless()")
+@DisabledIf("java.awt.GraphicsEnvironment.isHeadless()")
 @SpringBootTest
 class ProyectoApplicationTests {
 
