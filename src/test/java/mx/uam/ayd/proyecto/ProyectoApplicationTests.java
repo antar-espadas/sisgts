b55358f49125af2621364804f2ff8b54c71fbb7b
espadas-antar <espadas.antar@gmail.com>
2022-12-07 23:32:48 -0600

    Segundo intento

--- src/test/java/mx/uam/ayd/proyecto/ProyectoApplicationTests.java
+++ src/test/java/mx/uam/ayd/proyecto/ProyectoApplicationTests.java
@@ -2,19 +2,19 @@ package mx.uam.ayd.proyecto;
 
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.DisabledIf;
 import org.junit.jupiter.api.condition.DisabledIfSystemProperty;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.awt.*;
 
-@DisabledIfSystemProperty( named = "java.awt.headless", matches = "true")
+@DisabledIf("GraphicsEnvironment.isHeadless()")
 @SpringBootTest
 class ProyectoApplicationTests {
 
 	@BeforeAll
 	static void setUp(){
-		if (!GraphicsEnvironment.isHeadless())
-			System.setProperty("java.awt.headless", "false");
+		System.setProperty("java.awt.headless", "false");
 	}
 
 	@Test
