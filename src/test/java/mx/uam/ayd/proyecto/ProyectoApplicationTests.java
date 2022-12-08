373b1a3e05ab5d7d01eb34a962e1ebea7a176138
espadas-antar <espadas.antar@gmail.com>
2022-12-07 23:25:59 -0600

    Adapta pruebas para entorno headless

--- src/test/java/mx/uam/ayd/proyecto/ProyectoApplicationTests.java
+++ src/test/java/mx/uam/ayd/proyecto/ProyectoApplicationTests.java
@@ -1,10 +1,21 @@
 package mx.uam.ayd.proyecto;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.DisabledIfSystemProperty;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.awt.*;
+
+@DisabledIfSystemProperty( named = "java.awt.headless", matches = "true")
 @SpringBootTest
 class ProyectoApplicationTests {
+
+	@BeforeAll
+	static void setUp(){
+		if (!GraphicsEnvironment.isHeadless())
+			System.setProperty("java.awt.headless", "false");
+	}
 
 	@Test
 	void contextLoads() {
